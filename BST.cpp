4c9ff7a04a35c2e0fd8ea78816bf477717c3df77
rakeshKM <rakeshiitbbs@gmail.com>
2016-06-16 00:42:20 +0530

    Update BST.cpp

--- BST.cpp
+++ BST.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <queue>
+#include <cstdio>
 #include <cstddef>
 using namespace std;	
 class Node{
@@ -43,6 +45,45 @@ class BST{
             }else{
                  return rd+1;
              }                       
+        }
+        //height iterative
+        //iterative method
+        // Iterative method to find height of Bianry Tree
+        int getHeight_iteratively(Node* root){
+            // Base Case
+            if (root == NULL)
+                return 0;
+
+            // Create an empty queue for level order tarversal
+            queue<Node*> q;
+
+            // Enqueue Root and initialize height
+            q.push(root);
+            int height = 0;
+
+            while (1)
+            {
+                // nodeCount (queue size) indicates number of nodes
+                // at current lelvel.
+                int nodeCount = q.size();
+                if (nodeCount == 0)
+                    return height;
+
+                height++;
+
+                // Dequeue all nodes of current level and Enqueue all
+                // nodes of next level
+                while (nodeCount > 0)
+                {
+                    Node *tempNode = q.front();
+                    q.pop();
+                    if (tempNode->left != NULL)
+                        q.push(tempNode->left);
+                    if (tempNode->right != NULL)
+                        q.push(tempNode->right);
+                    nodeCount--;
+                }
+            }
         }
 };//End of BST
 int main(){
