39cdd0ef82bba19598d1df748cf323148340574a
Rakesh Kumar Mallik <rakeshiitbbs@gmail.com>
2016-10-19 10:34:08 +0530

    Update Graph_adjmat.cpp

--- Graph_adjmat.cpp
+++ Graph_adjmat.cpp
@@ -11,39 +11,39 @@ class Graph{
     private:
         int N;
         int **E;
+        void DFSutil(int vertex,bool *visited);
     public:
-    Graph(int vertex):N(vertex){
+        Graph(int vertex);
+        ~Graph();
+        bool insertEdge(int x,int y);
+        bool deleteEdge(int x,int y);
+        bool BFS(int startvertex);
+        bool DFS(int startvertex);
+};
+Graph::Graph(int vertex):N(vertex){
         E=new int*[N];
         for(int i=0;i<N;i++){
             E[i]=new int[N];
         }
         for(int i=0;i<N;i++){
             for(int j=0;j<N;j++){
-                E[i][j]= 0;
+                E[i][j]= -1;
             }
 	   }
     }
-    ~Graph(){
+Graph::~Graph(){
         for(int i=0;i<N;i++){
             delete [] E[i];
         }
         delete [] E;
         
     }
-     bool insertEdge(int x,int y){
+bool Graph::insertEdge(int x,int y){
          E[x][y]=1;
          E[y][x]=1;
          return true;         
     }
-    bool deleteEdge(int x,int y){
-         E[x][y]=0;
-         E[y][x]=0;
-        return true;
-    }
-    bool isEmpty(){
-         return false;
-    }
-    bool BFS(int startvertex){
+bool Graph::BFS(int startvertex){
         
         bool *visited=new bool[N];
         for (int k = 0; k < N; k++){         
@@ -59,7 +59,7 @@ class Graph{
             cout<<s<<' ';
             q.pop();
             for(int i=0;i<N;i++){
-                if(E[s][i]!=0 &&  visited[i]!=true){
+                if(E[s][i]!=-1 &&  visited[i]!=true){
                     q.push(i);
                     visited[i]=true;
                 }
@@ -67,24 +67,41 @@ class Graph{
         }
          return true;
     }
-    bool DFS(int startvertex,bool * visited){
-         return true;
-    }
-};
+void Graph::DFSutil(int vertex,bool *visited){
+            visited[vertex]=true;
+            cout<<vertex<<' ';
+            for(int i=0;i<N;i++){
+                if(E[vertex][i]!=-1 && visited[i] != true){
+                    DFSutil(i,visited);
+                }
+            }
+        }
+bool Graph::DFS(int startvertex){        
+        bool *visited=new bool[N];
+        for (int k = 0; k < N; k++){         
+            visited[k] = false;
+        }
+        DFSutil(startvertex,visited);
+        return true;
+}
 // Driver program to test methods of graph class
 int main()
 {
     // Create a graph given in the above diagram
-    Graph g(4);
+    Graph g(6);
     g.insertEdge(0, 1);
     g.insertEdge(0, 2);
-    g.insertEdge(1, 2);
-    g.insertEdge(2, 0);
-    g.insertEdge(2, 3);
-    g.insertEdge(3, 1);
+    g.insertEdge(1, 3);
+    g.insertEdge(2, 4);
+    //g.insertEdge(2, 3);
+    //g.insertEdge(3, 1);
+    g.insertEdge(3, 5);
+   // g.insertEdge(1, 5);
+    //g.insertEdge(0, 4);
  
     cout << "Following is Breadth First Traversal (starting from vertex 2) \n";
-    g.BFS(2);
- 
+    g.BFS(0);
+    cout << "\nFollowing is depth First Traversal (starting from vertex 2) \n";
+    g.DFS(0);
     return 0;
 }
